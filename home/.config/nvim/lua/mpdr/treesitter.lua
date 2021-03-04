1df300028018bb5ff289bcf025e781e37cf9f140
Moisés Rocha <moisesrocha99@gmail.com>
2021-03-04 17:22:21 +0000

    Disabled treesitter indenting

--- home/.config/nvim/lua/mpdr/treesitter.lua
+++ home/.config/nvim/lua/mpdr/treesitter.lua
@@ -1,9 +1,6 @@
 
 require'nvim-treesitter.configs'.setup {
   ensure_installed = { "cpp", "typescript", "javascript", "tsx" };
-  indent = {
-    enable = true,
-  };
   highlight = {
     enable = true,
   };
