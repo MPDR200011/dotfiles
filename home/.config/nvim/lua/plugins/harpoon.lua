0572d6c5dcc812d507bddd4b381c1535c53f53c0
Moisés Rocha <moisesrocha99@gmail.com>
2025-03-14 19:05:15 +0000

    [nvim] Remap harpoon binds

--- home/.config/nvim/lua/plugins/harpoon.lua
+++ home/.config/nvim/lua/plugins/harpoon.lua
@@ -11,10 +11,16 @@ return {
 
         local ui = harpoon.ui
 
-        vim.keymap.set('n', '<C-M>', function() harpoon:list():append() end, {})
-        vim.keymap.set('n', '<leader>hg', function() ui:toggle_quick_menu(harpoon:list()) end, {})
-        vim.keymap.set('n', '<C-H>', function() harpoon:list():prev() end, {})
-        vim.keymap.set('n', '<C-L>', function() harpoon:list():next() end, {})
+        vim.keymap.set('n', '<leader>a', function() harpoon:list():add() end, {})
+        vim.keymap.set('n', '<C-e>', function() ui:toggle_quick_menu(harpoon:list()) end, {})
+
+        vim.keymap.set('n', '<C-h>', function() harpoon:list():select(1) end, {})
+        vim.keymap.set('n', '<C-j>', function() harpoon:list():select(2) end, {})
+        vim.keymap.set('n', '<C-k>', function() harpoon:list():select(3) end, {})
+        vim.keymap.set('n', '<C-l>', function() harpoon:list():select(4) end, {})
+
+        vim.keymap.set('n', '<C-S-P>', function() harpoon:list():prev() end, {})
+        vim.keymap.set('n', '<C-S-N>', function() harpoon:list():next() end, {})
     end
 }
 
