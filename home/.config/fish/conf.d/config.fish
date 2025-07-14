faef7489eb99829d8b9dd45df56b14aa363ccc64
Moisés Rocha <moisesrocha99@gmail.com>
2025-07-14 21:46:51 +0100

    [fish] Move Android env variables
    For some reason they weren't applying when in the omf folder

--- home/.config/fish/conf.d/config.fish
+++ home/.config/fish/conf.d/config.fish
@@ -9,3 +9,10 @@ set -x XDG_CONFIG_DIRS "/etc/xdg"
 # config file cleanup
 set -x GNUPGHOME "$XDG_CONFIG_HOME/gnupg"
 set -x CCACHE_CONFIGPATH "$XDG_CONFIG_HOME/ccache"
+
+set x PATH "$PATH:$HOME/.gem/ruby/2.7.0/bin:$HOME/.local/bin:$HOME/bin"
+
+# Android stuff
+set -x ANDROID_HOME "/opt/android-sdk"
+set -x PATH "$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin"
+set -x GRADLE_HOME "/usr/share/java/gradle"
--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -13,11 +13,3 @@ abbr -a -- dup '$TERMINAL & && disown'
 function run_ansible_tasks
     ansible -m ansible.builtin.include_tasks -a $argv[1] -e dotfiles_path="$HOME/dotfiles" -K localhost  
 end
-
-set -x PATH "$PATH:$HOME/.gem/ruby/2.7.0/bin:$HOME/.local/bin:$HOME/bin"
-
-# Android stuff
-set -x ANDROID_HOME "$HOME/Android/Sdk"
-set -x ANDROID_SDK_ROOT "$HOME/Android/Sdk"
-set -x PATH "$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools"
-set -x GRADLE_HOME "/usr/share/java/gradle"
