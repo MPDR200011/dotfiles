df09b15a4def54564772cc608f7bce113d35c158
Moisés Rocha <moisesrocha99@gmail.com>
2025-07-21 23:54:25 +0100

    [android] Set android sdk home

--- home/.config/fish/conf.d/config.fish
+++ home/.config/fish/conf.d/config.fish
@@ -16,6 +16,7 @@ set -x PATH "$PATH:$HOME/.gem/ruby/2.7.0/bin:$HOME/.local/bin:$HOME/bin"
 set -x ANDROID_HOME "/opt/android-sdk"
 set -x PATH "$PATH:$ANDROID_HOME/emulator:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/cmdline-tools/latest/bin"
 set -x GRADLE_HOME "/usr/share/java/gradle"
+set -x ANDROID_SDK_HOME "$HOME"
 
 set -x PATH "$PATH:/home/mpdr/.local/share/JetBrains/Toolbox/scripts"
 
