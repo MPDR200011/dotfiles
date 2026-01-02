a40d871c07d5a25a9a014bd4b7950a873ad99e2f
Moisés Rocha <moisesrocha99@gmail.com>
2026-01-02 17:49:07 +0000

    [fish] Add pnpm cfg

--- home/.config/omf/init.fish
+++ home/.config/omf/init.fish
@@ -13,3 +13,10 @@ abbr -a -- dup '$TERMINAL & && disown'
 function run_ansible_tasks
     ansible -m ansible.builtin.include_tasks -a $argv[1] -e dotfiles_path="$HOME/dotfiles" -K localhost  
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/mpdr/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
