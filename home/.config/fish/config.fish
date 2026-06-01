682dea75f3322bfb888917c499adfd78b93b4ba4
Moisés Rocha <moisesrocha99@gmail.com>
2026-06-01 19:33:02 +0100

    [fish] Add ZVM ENV variables

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# ZVM
+set -gx ZVM_INSTALL "$HOME/.zvm/self"
+set -gx PATH $PATH "$HOME/.zvm/bin"
+set -gx PATH $PATH "$ZVM_INSTALL/"
